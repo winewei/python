e590e2a62c22dd857f219d16d36df5c5717033ae
tingping <937344288@qq.com>
2016-11-12 10:41:12 +0800

    hello world.

---
+++ tornado-web.py
@@ -0,0 +1,1 @@
+#!/usr/bin/evn python#import tornado.ioloopimport tornado.webclass MainHandler(tornado.web.RequestHandler):    def get(self):        self.write("Hello, world!")application = tornado.web.Application([    (r"/", MainHandler)])if __name__ == "__main__":    application.listen(8888,address="0.0.0.0")    tornado.ioloop.IOLoop.instance().start()    
